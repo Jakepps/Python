c88ccc4bd41bef2fda3445b6bcc5da6f7787d060
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-09-08 21:27:59 +0300

    ЛР2

--- #1.py
+++ #1.py
@@ -1,5 +1,16 @@
 #1
-b=int(input('b='))
-h=int(input('h='))
-S=(1/2)*b*h
-print('S=',S)
+#b=int(input('b='))
+##h=int(input('h='))
+#S=(1/2)*b*h
+#print('S=',S)
+#2
+g=int(input('g='))
+if g % 4!=0:
+    print('NO')
+elif g % 100 == 0:
+    if g % 400 == 0:
+        print('YES')
+    else:
+        print('NO')
+else:
+    print('YES')
