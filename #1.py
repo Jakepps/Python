00c6c7928d613a35e735f53f435ebe0600cf2de2
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-09-08 22:17:06 +0300

    ЛР3

--- #1.py
+++ #1.py
@@ -1,16 +1,21 @@
+import math
 #1
 #b=int(input('b='))
 ##h=int(input('h='))
 #S=(1/2)*b*h
 #print('S=',S)
 #2
-g=int(input('g='))
-if g % 4!=0:
-    print('NO')
-elif g % 100 == 0:
-    if g % 400 == 0:
-        print('YES')
-    else:
-        print('NO')
-else:
-    print('YES')
+#g=int(input('g='))
+#if g % 4!=0:
+ #   print('NO')
+#elif g % 100 == 0:
+   # if g % 400 == 0:
+  #      print('YES')
+ #   else:
+#        print('NO')
+#else:
+#    print('YES')
+#3
+n=int(input('n='))
+m=int(input('m='))
+print(round(m/n))
