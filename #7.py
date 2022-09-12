5dbdd247c87bd6a37f462c963b958eb9a946c84a
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-09-12 13:06:35 +0300

    ЛР8

--- #7.py
+++ #7.py
@@ -1,5 +1,13 @@
 #7
-a=[int(i) for i in input().split()]
-for i in range(1,len(a)):
-    if a[i] > a[i-1]:
-        print(a[i])
+#a=[int(i) for i in input().split()]
+#for i in range(1,len(a)):
+#    if a[i] > a[i-1]:
+#        print(a[i])
+#8
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print(fib(int(input())))
