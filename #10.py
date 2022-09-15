cf226fe7a075203b777cb993c637050781bdfb55
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-09-15 22:43:30 +0300

    ЛР11

--- #10.py
+++ #10.py
@@ -1,16 +1,33 @@
 #10
 #нашел прикольную функцию вывода
-def print_set(some_set):
-    print(len(some_set))
-    print(*[str(item) for item in sorted(some_set)])
- 
-N, M = [int(s) for s in input().split()]
-A_colors, B_colors = set(), set()
-for i in range(N):
-    A_colors.add(int(input()))
-for i in range(M):
-    B_colors.add(int(input()))
+#def print_set(some_set):
+#    print(len(some_set))
+#    print(*[str(item) for item in sorted(some_set)]) 
+#N, M = [int(s) for s in input().split()]
+#A_colors, B_colors = set(), set()
+#for i in range(N):
+#    A_colors.add(int(input()))
+#for i in range(M):
+#    B_colors.add(int(input()))
      
-print_set(A_colors & B_colors)
-print_set(A_colors - B_colors)
-print_set(B_colors - A_colors)
+#print_set(A_colors & B_colors)
+#print_set(A_colors - B_colors)
+#print_set(B_colors - A_colors)
+#11
+actions = {
+    'read': 'R',
+    'write': 'W',
+    'execute': 'X',
+}
+ 
+file_permissions = {}
+for i in range(int(input())):
+    file, *permissions = input().split()
+    file_permissions[file] = set(permissions)
+ 
+for i in range(int(input())):
+    action, file = input().split()
+    if actions[action] in file_permissions[file]:
+        print('OK')
+    else:
+        print('Access denied')
