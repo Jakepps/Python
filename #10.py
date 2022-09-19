863d9fe90d8885446a9b93c02e884f400547a76b
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-09-19 16:49:14 +0300

    ЛР12

--- #10.py
+++ #10.py
@@ -1,3 +1,5 @@
+import string
+string.punctuation
 #10
 #нашел прикольную функцию вывода
 #def print_set(some_set):
@@ -14,20 +16,45 @@
 #print_set(A_colors - B_colors)
 #print_set(B_colors - A_colors)
 #11
-actions = {
-    'read': 'R',
-    'write': 'W',
-    'execute': 'X',
-}
+#ACTION_PERMISSION = {
+#    'read': 'R',
+#    'write': 'W',
+#    'execute': 'X',
+#}
  
-file_permissions = {}
-for i in range(int(input())):
-    file, *permissions = input().split()
-    file_permissions[file] = set(permissions)
+#file_permissions = {}
+#for i in range(int(input())):
+#    file, *permissions = input().split()
+#    file_permissions[file] = set(permissions)
  
-for i in range(int(input())):
-    action, file = input().split()
-    if actions[action] in file_permissions[file]:
-        print('OK')
-    else:
-        print('Access denied')
+#for i in range(int(input())):
+#    action, file = input().split()
+#    if ACTION_PERMISSION[action] in file_permissions[file]:
+#        print('OK')
+#    else:
+#        print('Access denied')
+#12
+namefile='C:/Users/Артём/source/repos/Jakepps/Python/test.txt'
+num_words = 0
+with open(namefile, 'r') as f:
+    for line in f:
+        words = line.split()
+        num_words += len(words)
+
+with open(namefile) as f:
+    flat_list=[word for line in f for word in line.split()]
+out=(" ".join(flat_list))
+
+for p in string.punctuation:
+    if p in out:
+        out=out.replace(p,'')
+
+counter = {}
+for i in range(num_words):
+    line = out.split()
+    for word in line:
+        counter[word] = counter.get(word, 0) + 1
+         
+max_count = max(counter.values())
+most_frequent = [k for k, v in counter.items() if v == max_count]
+print(min(most_frequent))
