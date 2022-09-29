01cc162628111162702560d480ee91a34d29f2e0
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-09-29 22:23:03 +0300

    12-9

---
+++ #12-9.py
@@ -0,0 +1,28 @@
+#12-9
+def choice(grade):
+    if grade=='A' or grade=='a':
+        print("Оценка 5")
+    elif grade=='B' or grade=='b':
+        print("Оценка 4")
+    elif grade=='C' or grade=='c':
+        print("Оценка 3")
+    elif grade=='D' or grade=='d':
+        print("Оценка 2")
+    elif grade=='F' or grade=='f':
+        print("Отчислен.")
+    if grade=='5' or grade=='5':
+        print("Оценка A")
+    elif grade=='4' or grade=='4':
+        print("Оценка B")
+    elif grade=='3' or grade=='3':
+        print("Оценка C")
+    elif grade=='2' or grade=='2':
+        print("Оценка D")
+    elif grade=='Отчислен' or grade=="отчислен":
+        print("Оценка F.")
+
+while True:
+    g=input("Ввидите оценку=")
+    print(choice(g))
+    if not g:
+        break  
